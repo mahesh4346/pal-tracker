ef2bdca8a75e6254985eee71d0971a9ee1103cfa
Mahesh Gopala <mahesh.gopal@wipro.com>
2019-12-02 14:53:30 -0700

    text

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String param;
+    public WelcomeController(@Value("${welcome.message}") String param)
+    {
+        this.param = param;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return param;
     }
 }
